a205997a8065779990f0df0e2fee8e0444429b59
n00bhaxor <43417886+n00bhaxor@users.noreply.github.com>
2022-09-24 09:28:05 -0400

    Update installSalt.sh

--- installSalt.sh
+++ installSalt.sh
@@ -38,10 +38,10 @@ if [ $osType = "DEB" ]; then
     sudo curl -fsSL -o /usr/share/keyrings/salt-archive-keyring.gpg https://repo.saltproject.io/py3/debian/11/amd64/3004/salt-archive-keyring.gpg
     #wget -O - https://repo.saltstack.com/apt/debian/9/amd64/latest/SALTSTACK-GPG-KEY.pub | sudo apt-key add - | tee -a $logFile
     #echo "deb http://repo.saltstack.com/apt/debian/9/amd64/latest stretch main" >> /etc/apt/sources.list.d/saltstack.list | tee -a $logFile
-    echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/debian/11/amd64/3004 bullseye main" | sudo tee /etc/apt/sources.list.d/salt.list
-    apt-get update
+    sudo echo "deb [signed-by=/usr/share/keyrings/salt-archive-keyring.gpg arch=amd64] https://repo.saltproject.io/py3/debian/11/amd64/3004 bullseye main" | sudo tee /etc/apt/sources.list.d/salt.list
+    sudo apt-get update
     #apt-get -y install gcc-8-base -o Dpkg::Options::=--force-confnew
-    apt-get -y install salt-minion
+    sudo apt-get -y install salt-minion
     numPkgs=`dpkg -l | grep salt | grep ^ii | wc -l`
     if [ $numPkgs -eq 2 ]; then
         echo "Salt installed successfully" >> $logFile
